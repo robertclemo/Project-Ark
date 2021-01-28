017adca4a5ea640895b9ae0f7d5c6770c096921c
robertclemo <72224419+robertclemo@users.noreply.github.com>
2021-01-27 21:04:00 -0500

    Added cats and dogs queries
    These build the tables.

---
+++ cats.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS	members
+(
+userID int PRIMARY KEY AUTO_INCREMENT,
+username varchar(10) NOT NULL,
+pass varchar(10) NOT NULL
+
+);
+
+CREATE TABLE members (userID int PRIMARY key AUTO_INCREMENT, username varchar(10) NOT NULL, pass varchar(10) NOT NULL)
+
+______________
+
+create table cats (name text not null, age int(3) not null, catcondition text not null, catmedicine text not null, temprement text not null, adoptable text not null)
+
+
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Sprinkles", "2", "Stable", "Up To Date", "Friendly", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Sappho", "1", "Stable", "Up To Date", "Shy", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Lucipurr", "5", "Distemper", "Prozac", "Spiteful", "No")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Kitty Kitty", "6", "Stable", "Up To Date", "Curious", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Fuzzle", "1", "Stable", "Up To Date", "Friendly", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Cinnamon", "14", "UTI", "Enrofloxacin", "Friendly", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Bella", "8", "Skin Lesion", "Amoxicillin", "Shy", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Rigby", "14", "Stable", "Up To Date", "Friendly", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Shamrock", "12", "Cold", "Amoxicillin", "Shy", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Sneakthief", "6", "Stable", "Up To Date", "Mischievous", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Pumpkin", "7", "Diabetes", "Glargine", "Friendly", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Eeyore", "15", "Depression", "Trazodone", "Depressed", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Bubba", "8", "Stable", "Up To Date", "Friendly", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Bjorn", "10", "Stable", "Up To Date", "Friendly", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Noel", "12", "Stable", "Up To Date", "Friendly", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Trixie", "3", "Stable", "Up To Date", "Friendly", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Ginger", "4", "Stable", "Up To Date", "Friendly", "Yes")
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("Pandora Ban", "4", "Stable", "Up To Date", "Mischievous", "Yes")
+
+
+insert into cats (name, age, catcondition, catmedicine, temprement, adoptable) values ("
+
+drop table cats
+
+select * from cats
---
+++ dogs.sql
@@ -0,0 +1,10 @@
+create table dogs (name text not null, age int(3) not null, dogcondition text not null, dogmedicine text not null, temprement text not null, adoptable text not null)
+
+insert into dogs (name, age, dogcondition, dogmedicine, temprement, adoptable) values ("Izzy", "3", "Stable", "Up To Date", "Friendly", "Yes")
+insert into dogs (name, age, dogcondition, dogmedicine, temprement, adoptable) values ("Scamander", "8", "Diabetes", "Insulin", "Lazy", "Yes")
+insert into dogs (name, age, dogcondition, dogmedicine, temprement, adoptable) values ("Regulus", "4", "Stable", "Up To Date", "Service", "Yes")
+insert into dogs (name, age, dogcondition, dogmedicine, temprement, adoptable) values ("Zefnar", "7", "Stable", "Up To Date", "Guard", "Yes")
+insert into dogs (name, age, dogcondition, dogmedicine, temprement, adoptable) values ("Scraps", "1", "Ear Lesion", "Amoxicillin", "Friendly", "Yes")
+insert into dogs (name, age, dogcondition, dogmedicine, temprement, adoptable) values ("
+
+
